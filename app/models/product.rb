8685d620ba1641220d15fb6dd67f81f570b5b573
UniqueRobert <robertbroll@gmail.com>
2015-04-19 17:44:50 +0100

    fixed upload images

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
